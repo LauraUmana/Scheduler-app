7e93d82938b9f0b7f5f95fbb6c1d35cbfc531d07
Laura Umana <laurau1107@gmail.com>
2015-06-10 22:41:32 -0400

    restructured my code and files

--- js/app.js
+++ js/app.js
@@ -3,45 +3,55 @@
 'use strict';
 
 angular.module('schedulerApp', [])
-.controller('SchedulerController', function($scope) {
-	$scope.plans = [
-	{
-		displayTime: '05:00pm',
-		hours: '05',
-		minutes: '00',
-		meridiem: 'pm',
-		standardTime: 1700,
-		duration: '30',
-		task: 'Team meeting',
-	},
-	{
-		displayTime: '06:00pm',
-		hours: '06',
-		minutes: '00',
-		meridiem: 'pm',
-		standardTime: 1800,
-		duration: '60',
-		task: 'Watch a js talk online'
-	},
-	];
+.controller('SchedulerController', schedulerCtrl);
 
+	function schedulerCtrl() {
+		this.plans = [
+		{
+			displayTime: '05:00pm',
+			hours: '05',
+			minutes: '00',
+			meridiem: 'pm',
+			standardTime: 1700,
+			duration: '30',
+			task: 'Team meeting',
+		},
+		{
+			displayTime: '06:00pm',
+			hours: '06',
+			minutes: '00',
+			meridiem: 'pm',
+			standardTime: 1800,
+			duration: '60',
+			task: 'Watch a js talk online'
+		},
+		];
+	}
 	
-	$scope.addNewTask = function() {
-		$scope.user.displayTime = $scope.user.hours + ":" + $scope.user.minutes + $scope.user.meridiem;
-		
+	schedulerCtrl.prototype.addNewTask = function() {
+		var that = this;
+
+		that.user.displayTime = that.user.hours + ":" + that.user.minutes + that.user.meridiem;
+		console.log(that.user.displayTime);
 
 		var timeFound = false; 
-			for( var i = 0; i < $scope.plans.length; i++) {
-				console.log($scope.user.displayTime);
-				console.log($scope.plans[i].displayTime);
-				if ( $scope.user.displayTime === $scope.plans[i].displayTime) {
+			for( var i = 0; i < that.plans.length; i++) {
+				console.log(that.user.displayTime);
+				console.log(that.plans[i].displayTime);
+				if ( that.user.displayTime === that.plans[i].displayTime) {
 					alert("You can't enter the same time twice.");
 					timeFound = true;
 				}
 				if(timeFound) return;
 			}
+
+		that.plans.push(that.user);
+		that.user = '';
+		console.log(that.plans);
+	}
+
 		
-			/*function convert(inputTime){
+		/*var convert = function(inputTime) {
 			var hours = parseInt(inputTime.substr(0,2));
 			console.log(hours);
 			if(inputTime.indexOf('am') && hours == 12) {
@@ -56,12 +66,12 @@ angular.module('schedulerApp', [])
 		}*/
 
 		
-		$scope.plans.push($scope.user);
+		/*$scope.plans.push($scope.user);
 		$scope.user = '';
 		console.log($scope.plans);
 	}
 
-		/*$scope.plans.sort(function (a, b) {
+		$scope.plans.sort(function (a, b) {
 			console.log(a);
 			if (a > b) {
 				return 1;
@@ -69,5 +79,5 @@ angular.module('schedulerApp', [])
 			if (a < b) {
 				return -1;
 			}
-		});*/
-});
+		});
+});*/
